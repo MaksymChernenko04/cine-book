5678057dcac3a1c76fac8a33e658e37531b1a75d
Maksym <chernenkomax04@gmail.com>
2025-09-19 20:45:47 +0200

    Initial commit

---
+++ src/main/java/com/maksymchernenko/cinebook/CineBookApplication.java
@@ -0,0 +1,13 @@
+package com.maksymchernenko.cinebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineBookApplication.class, args);
+	}
+
+}

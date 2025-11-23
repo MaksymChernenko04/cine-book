519112afc53b2f7685d8b60a20bb5d61d229dc0e
Maksym Chernenko <chernenkomax04@gmail.com>
2025-11-23 20:45:12 +0100

    Refactor hall model

--- src/main/java/com/maksymchernenko/cinebook/model/Hall.java
+++ src/main/java/com/maksymchernenko/cinebook/model/Hall.java
@@ -1,9 +1,6 @@
 package com.maksymchernenko.cinebook.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.util.List;
@@ -20,16 +17,12 @@ public class Hall {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @NotBlank(message = "Hall name cannot be blank")
     private String name;
 
-    @NotNull(message = "Hall capacity cannot be null")
-    @Min(value = 1, message = "Hall capacity must be a positive number")
     private Integer capacity;
 
     @OneToMany(mappedBy = "hall", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Seat> seats;
 
-    @NotNull(message = "Hall accessibility flag must be not null")
     private Boolean accessibility = false;
 }

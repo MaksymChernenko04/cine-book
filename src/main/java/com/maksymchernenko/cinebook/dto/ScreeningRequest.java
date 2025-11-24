6e187035cb06e1444251fec70b6adaf021260354
Maksym Chernenko <chernenkomax04@gmail.com>
2025-11-24 17:42:42 +0100

    Fix ScreeningRequest

--- src/main/java/com/maksymchernenko/cinebook/dto/ScreeningRequest.java
+++ src/main/java/com/maksymchernenko/cinebook/dto/ScreeningRequest.java
@@ -1,9 +1,7 @@
 package com.maksymchernenko.cinebook.dto;
 
-import com.maksymchernenko.cinebook.model.Hall;
 import com.maksymchernenko.cinebook.model.Movie;
 import com.maksymchernenko.cinebook.model.Screening;
-import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,21 +17,15 @@ import java.time.LocalDateTime;
 public class ScreeningRequest {
 
     @NotNull(message = "Screening movie must be not null")
-    @ManyToOne(optional = false)
     private Movie movie;
 
     @NotNull(message = "Screening start time must be not null")
     private LocalDateTime startTime;
 
-    @NotNull(message = "Screening hall must be not null")
-    @ManyToOne(optional = false)
-    private Hall hall;
-
     public static Screening toEntity(ScreeningRequest screeningRequest) {
         return Screening.builder()
                 .movie(screeningRequest.getMovie())
                 .startTime(screeningRequest.getStartTime())
-                .hall(screeningRequest.getHall())
                 .build();
     }
 }
